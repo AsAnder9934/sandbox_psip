dac5ef527301752d1038db16a8425190460bcac5
wojciecholeksy <149381966+wojciecholeksy@users.noreply.github.com>
2023-11-20 13:29:38 +0100

    feat-update_modify_function
    Co-Authored-By: AsAnder9934 <149382112+AsAnder9934@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from dane import users_list
 from utils.functions import add_user_to, remove_user_from, show_users_from, gui
 
-# add_user_to(users_list)
-# remove_user_from(users_list)
-# def show_users_from(users_list:list)->None:
-#     for user in users_list:
-#         print(f'Twój znajomy {user["name"]} dodał {user["posts"]} postów')
 
 gui(users_list)
